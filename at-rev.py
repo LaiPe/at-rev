368367bcc9c7706cc218560e01cfaf32bb69ad58
Léo Desktop <peyronnet.leo@gmail.com>
2023-11-03 21:28:44 +0100

    environnement viruel et liste des dépendences

--- at-rev.py
+++ at-rev.py
@@ -5,24 +5,23 @@ def extraction(texte):
     soupe = BeautifulSoup(texte, "html.parser")
     td_list = soupe.find_all('td') #Ensemble des tags <td> de la page
     content_list = [] #Ensemble du contenu des tags <td> impairs de la page
-    for i in range(len(td_list)):
-        if i%2 == 1: 
-            content_list += [td_list[i].string]
+    for i in range(1,len(td_list),2):
+        content_list += [td_list[i].string]
     return content_list 
 
 def reconstruction(li):
     li_reconstruct = [] 
     temp = ""
-    for i in range(len(li)):
-        selector = li[i][len(li[i])-1] #Dernier caractère de la ligne
-        caract_final = selector == "." or selector == "?" or selector == "!" 
-        didascalies = li[i][0] == "(" and selector == ")"
-        if caract_final or didascalies: #Si le dernier caractère de la ligne est une "fin de phrase"
-            temp += li[i]
+    for ligne in li:
+        last_car = ligne[len(ligne)-1] #Dernier caractère de la ligne
+        caract_final = last_car == "." or last_car == "?" or last_car == "!" 
+        didascalies = ligne[0] == "(" and last_car == ")"
+        if caract_final or didascalies: #Si cette ligne constitue une fin de phrase
+            temp += ligne
             li_reconstruct += [temp]
             temp = ""
         else:
-            temp += li[i] + " "
+            temp += ligne + " "
     return li_reconstruct
 
 def compta_caracteres(li):
@@ -31,7 +30,28 @@ def compta_caracteres(li):
         result += len(e)
     return result
 
-#def traduction(li):
+def traduction(li):
+    li_trad = []
+    for e in li:
+        li_trad += [appel_api(e)]
+    return li_trad
+
+def modifications(li,dict):
+    li_modif = []
+    temp = ""
+    cpt = 0
+    for chain in li:
+        for letter in chain:
+            if letter in dict:
+                cpt += 1
+                temp += dict[letter]
+            else:
+                temp += letter
+        li_modif += [temp]
+        temp = ""
+    print("\n=> ",cpt," modifs effectués\n",sep="")
+    return li_modif
+
 
 def ecriture(w,li):
     for e in li:
@@ -61,8 +81,12 @@ if __name__ == "__main__":
     print("- Reconstruction")
 
     print("\n=> ",compta_caracteres(liste_phrases_vo)," caractères envoyées à l'API Google Translate\n",sep="")
-    ecriture(w,liste_phrases_vo)
 
+    list_phrases_trad_modif = modifications(liste_phrases_vo,{"’":"'"})
+    print("- Modification")
+
+    ecriture(w,list_phrases_trad_modif)
+    print("- Ecriture")
 
 
 
