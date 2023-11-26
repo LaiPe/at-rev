a7f022e01b7ae3916295ce15e3223704331e260f
Léo Desktop <peyronnet.leo@gmail.com>
2023-11-26 11:31:58 +0100

    Nouvelles conditions de retour à la ligne

--- fonctions.py
+++ fonctions.py
@@ -14,9 +14,13 @@ def reconstruction(li):
     for ligne in li:
         last_car = ligne[len(ligne)-1] #Dernier caractère de la ligne
         alast_car = ligne[len(ligne)-2] #Avant-dernier caractère de la ligne
-        caract_final = last_car == "." or last_car == "?" or last_car == "!" or (last_car == '"' and alast_car == ".")
-        didascalies = ligne[0] == "(" and last_car == ")"
-        if caract_final or didascalies: #Si cette ligne constitue une fin de phrase
+        aalast_car = ligne[len(ligne)-3] #Avant-avant-dernier caractère de la ligne
+
+        caract_final = last_car == "." or last_car == "?" or last_car == "!" or last_car == ")"
+        guillemets_final = last_car == '"' and (alast_car == "." or alast_car == "?" or alast_car == "!")
+        points_suspens = last_car == "." and alast_car == "." and aalast_car == "."
+        
+        if caract_final or guillemets_final or points_suspens: #Si cette ligne constitue une fin de phrase
             temp += ligne
             li_reconstruct += [temp]
             temp = ""
